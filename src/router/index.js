9b546735db6019ae21ac0585f713dc009d8bd0f2
abseruddin <abseruddin26@gmail.com>
2021-04-10 14:41:53 +0600

    Changed routing configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Relevant from '../views/search/Relevant.vue'
-import Search from "../views/search/Search";
 import User from "../views/user/User";
-import Auth from "../views/auth/Auth";
 import SignIn from "../views/auth/SignIn";
 
 import store from "../store"
@@ -12,33 +10,20 @@ Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/search',
-        component: Search,
-        name: 'Search',
-        children: [
-            {
-                path: 'relevant',
-                component: Relevant,
-                name: 'Relevant'
-            },
-        ]
+        path: '/search/relevant',
+        component: Relevant,
+        name: 'Relevant',
     },
+
     {
         path: '/user/:id',
         component: User,
         name: 'User'
     },
     {
-        path: '/auth',
-        component: Auth,
-        name: 'Auth',
-        children: [
-            {
-                path: 'signIn',
-                component: SignIn,
-                name: 'SignIn'
-            }
-        ]
+        path: '/auth/signIn',
+        component: SignIn,
+        name: 'SignIn',
     },
     {
         path: '/about',
