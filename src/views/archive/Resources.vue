2b4be54b4ac7e141bccd509a21b522ea1c118f3a
navidh86 <navidhasan0@gmail.com>
2021-04-13 13:02:00 +0600

    Create a ResourceCard component and use it in Resources view

--- src/views/archive/Resources.vue
+++ src/views/archive/Resources.vue
@@ -1,17 +1,33 @@
 <template>
   <v-col
     cols="12"
-    sm="8"
+    md="8"
   >
-    The Resources Page
-
-    <router-link v-for="batch in 5" :key="batch" route :to="`/archive/resource/${batch}`">{{batch}}</router-link>
+    Resource Explorer
+    <v-container class="my-5">
+      <v-row>
+        <v-col
+            :cols="$isMobile() ? '12' : '6'"
+            :md="$isMobile() ? '12' : '4'"
+            class="justify-center-center"
+            v-for="batch in 7"
+            :key="batch"
+        >
+          <resource-card type="folder">Batch {{ batch + 12 }} Resources</resource-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-col>
 </template>
 
 <script>
+import ResourceCard from "@/components/ResourceCard";
+import mixins from '@/mixins/index'
+
 export default {
-  name: "Resources"
+  name: "Resources",
+  components: {ResourceCard},
+  mixins: [mixins]
 }
 </script>
 
