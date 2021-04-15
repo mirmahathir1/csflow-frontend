58ecf018c3e1b762a97afa30f128c6c06568844c
navidh86 <navidhasan0@gmail.com>
2021-04-15 20:28:09 +0600

    Create a DetailsLoader component

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,16 @@
 <template>
   <v-card flat class="rounded-lg pa-2 my-2" min-height="60">
     <v-card-text class="font-weight-normal text-h6 black--text my-1 ml-3">
+      <v-hover v-slot:default="{ hover }" v-if="backButton">
+        <v-icon
+            class="black--text mr-3"
+            :class="{ 'my-hover': hover, 'grey--text': hover}"
+            size="30"
+            @click="$router.push(backRoute)"
+        >
+          mdi-arrow-left-thick
+        </v-icon>
+      </v-hover>
       <slot>Page Title</slot>
     </v-card-text>
   </v-card>
@@ -8,10 +18,22 @@
 
 <script>
 export default {
-  name: "PageHeader"
+  name: "PageHeader",
+  props: {
+    backButton: {
+      type: Boolean,
+      default: false
+    },
+    backRoute: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.my-hover {
+  cursor: pointer;
+}
 </style>
