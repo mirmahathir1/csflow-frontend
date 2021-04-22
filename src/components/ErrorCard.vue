8b4a051558f432b719f8ea77cd6db8fd23fe0f60
navidh86 <navidhasan0@gmail.com>
2021-04-22 14:08:13 +0600

    Create an ErrorCard component

---
+++ src/components/ErrorCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="text-center pt-7 mx-auto" height="120" max-width="600" elevation="1">
+    <v-card-text large class="rounded-lg black--text font-weight-normal text-h6">
+      <slot></slot>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ErrorCard"
+}
+</script>
+
+<style scoped>
+
+</style>
