42b98fa5eb1700cd1c210172c16ce26c4092302b
abseruddin <abseruddin26@gmail.com>
2021-05-11 13:08:16 +0600

    authentication module

--- src/views/auth/SignupComplete.vue
+++ src/views/auth/SignupComplete.vue
@@ -1,7 +1,7 @@
 <template>
   <PaddedContainer>
     <v-card :height="$isMobile() ?'100px':'100px'" class></v-card>
-    <v-card height="100px" class="mb-4 mx-auto"
+    <v-card height="100px" class="mb-4 mt-4 mx-auto"
       :max-width="$isMobile()?'30vw':'30vw'"
       :min-width="$isMobile()?'30vw':'30vw'">
       <v-row :class="$isMobile() ?'pa-2':'pa-5 mt-2'">
@@ -11,6 +11,17 @@
           </p>
         </v-card-text>
       </v-row>
+      <v-row class="justify-content-center">
+            <v-btn
+                color="primary"
+                class="ma-2"
+                @click="$router.push('/auth/signIn')"
+                rounded
+                v-show="isSuccess"
+            >
+            Sign In
+            </v-btn>
+        </v-row>
     </v-card>
     <v-card :height="$isMobile() ?'100px':'100px'" class="mb-4"></v-card>
   </PaddedContainer>
@@ -25,7 +36,8 @@ export default {
         return "Confirm Signup"
     },
     data:()=>({
-        message:"Confirming sign up ..."
+        message:"Confirming sign up ...",
+        isSuccess:false,
     }),
     components:{
         PaddedContainer
@@ -36,7 +48,9 @@ export default {
     mounted(){
         this.completeSignup({token: this.$route.query.token})
             .then(response=>{
-                this.$router.push('/auth/signIn')
+                // this.$router.push('/auth/signIn')
+                this.message="Success!!Log in to continue"
+                this.isSuccess=true
             }).catch(e=>{
                 this.message=e.response.data.message;
             })
