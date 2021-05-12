0a4238a496834282a8ba700a98545acc8de603cc
navidh86 <navidhasan0@gmail.com>
2021-05-12 13:45:59 +0600

    Show a loading screen while trying to auto login

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
         <Header></Header>
 <!--        <SideBar></SideBar>-->
         <v-main class="grey lighten-3">
-          <router-view></router-view>
+          <router-view v-if="!showSignInDialog"></router-view>
+          <sign-in-dialog v-else></sign-in-dialog>
         </v-main>
     </v-app>
 </template>
@@ -15,12 +16,16 @@
 import AppBar from "./components/AppBar";
 import Header from "./components/Header"
 import SideBar from "./components/SideBar"
+import SignInDialog from "@/components/SignInDialog";
 export default {
     name: 'App',
     computed:{
-
+      showSignInDialog() {
+        return this.$store.getters['auth/getSignInDialogFlag'];
+      }
     },
     components: {
+      SignInDialog,
         AppBar,
         Header,
         SideBar
