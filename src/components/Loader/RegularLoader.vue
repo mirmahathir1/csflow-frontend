bf3ba3e147a899b670987030a536ae41c092f4d5
navidh86 <navidhasan0@gmail.com>
2021-05-17 13:55:03 +0600

    Create a regular loader

---
+++ src/components/Loader/RegularLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-skeleton-loader
+      class="mx-auto text-center"
+      type="image"
+  ></v-skeleton-loader>
+</template>
+
+<script>
+export default {
+  name: "RegularLoader",
+}
+</script>
+
+<style scoped>
+
+</style>
