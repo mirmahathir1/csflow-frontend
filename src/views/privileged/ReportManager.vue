8edeb3fc7d452736a0bbdf9f8621adf0dbee01e7
navidh86 <navidhasan0@gmail.com>
2021-05-18 14:56:39 +0600

    Complete the ReportManger/Comment view, resolve and delete do not work yet

--- src/views/privileged/ReportManager.vue
+++ src/views/privileged/ReportManager.vue
@@ -21,7 +21,19 @@
                   <v-chip color="blue lighten-5 primary--text text-darken-1">
                     {{ formatDate(report['createdAt']) }}
                   </v-chip>
-                  <v-card-text>{{ report['description'] }}</v-card-text>
+                  <v-card-text>
+                    {{ truncate(report['description'], report['commentId']) }}
+                    <span
+                        v-if="report['description'].length > maxLength && !showFull[report['commentId']]"
+                        class="primary--text my-hover font-weight-bold text-caption"
+                        @click="setFull(report['commentId'])"
+                    >See More</span>
+                    <span
+                        v-if="report['description'].length > maxLength && showFull[report['commentId']]"
+                        class="primary--text my-hover font-weight-bold text-caption"
+                        @click="showFull = {}"
+                    >See Less</span>
+                  </v-card-text>
                 </v-col>
                 <v-col cols="12" md="6" class="py-0">
                   <user-card
@@ -31,17 +43,15 @@
                       :karma="parseInt(report['owner']['karma'])"
                       :image="report['owner']['profilePic']"
                       @click.native="$router.push('/user/' + report['owner']['studentId'])"
-                  >
-
-                  </user-card>
+                  ></user-card>
                 </v-col>
               </v-row>
             </v-card>
           </v-col>
           <v-col cols="12" class="text-center">
             <v-row class="justify-center">
-              <v-btn class="mx-3 my-2 primary">Resolve</v-btn>
-              <v-btn class="mx-3 my-2 red lighten-1 white--text">Delete</v-btn>
+              <v-btn class="mx-3 my-2 primary" @click="onResolveClicked(report['commentId'])">Resolve</v-btn>
+              <v-btn class="mx-3 my-2 red lighten-1 white--text" @click="onDeleteClicked(report['commentId'])">Delete</v-btn>
               <v-btn class="mx-3 my-2 no-click" tile color="blue lighten-5">
                 {{report['reportCount']}} Report{{ report['reportCount'] > 1 ? 's' : '' }}
               </v-btn>
@@ -53,7 +63,7 @@
           <v-col cols="12" class="px-0">
             <div class="mx-auto mb-3 mt-5">
               <v-pagination
-                  v-model="page.comment"
+                  v-model="page"
                   :length="commentPages"
                   circle
                   :total-visible="$isMobile() ? 6 : 7"
@@ -61,6 +71,88 @@
             </div>
           </v-col>
         </v-row>
+
+<!--        Resolve Dialog-->
+        <v-dialog
+            v-model="resolveDialog"
+            max-width="320"
+        >
+          <v-card>
+            <v-card-title class="headline">
+              Resolve this comment?
+            </v-card-title>
+            <v-card-text>Confirming this will keep the comment, but delete all the reports on this comment.</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="green darken-1"
+                  text
+                  @click="resolveDialog = false"
+              >
+                No
+              </v-btn>
+              <v-btn
+                  color="red darken-1"
+                  text
+                  @click="onResolveConfirmed"
+                  :loading="false"
+                  :disabled="false"
+              >
+                Yes
+              </v-btn>
+            </v-card-actions>
+            <v-row class="justify-center" v-if="false && submitted">
+              <v-alert
+                  type="error"
+                  outlined
+                  dense
+              >
+                message here
+              </v-alert>
+            </v-row>
+          </v-card>
+        </v-dialog>
+
+<!--        Delete Dialog-->
+        <v-dialog
+            v-model="deleteDialog"
+            max-width="320"
+        >
+          <v-card>
+            <v-card-title class="headline">
+              Delete this comment?
+            </v-card-title>
+            <v-card-text>Confirming this will remove the comment and all the reports on this comment.</v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="green darken-1"
+                  text
+                  @click="deleteDialog = false"
+              >
+                No
+              </v-btn>
+              <v-btn
+                  color="red darken-1"
+                  text
+                  @click="onDeleteConfirmed"
+                  :loading="false"
+                  :disabled="false"
+              >
+                Yes
+              </v-btn>
+            </v-card-actions>
+            <v-row class="justify-center" v-if="false && submitted">
+              <v-alert
+                  type="error"
+                  outlined
+                  dense
+              >
+                message here
+              </v-alert>
+            </v-row>
+          </v-card>
+        </v-dialog>
       </template>
       <details-loader v-else></details-loader>
     </v-card>
@@ -82,11 +174,14 @@ export default {
   data() {
     return {
       pageSize: 3,
-      page: {
-        post: 1,
-        answer: 1,
-        comment: 1,
-      },
+      page: 1,
+      resolveDialog: false,
+      resolveItem: -1,
+      deleteDialog: false,
+      deleteItem: -1,
+      submitted: false,
+      maxLength: 220,
+      showFull: {},
     };
   },
   computed: {
@@ -101,8 +196,10 @@ export default {
 
       return [];
     },
-    commentsCurrentPage(type) {
-      return this.comments.slice((this.page.comment-1)*this.pageSize, this.page.comment*this.pageSize);
+    commentsCurrentPage() {
+      this.page = Math.min(this.page, this.commentPages);
+
+      return this.comments.slice((this.page-1)*this.pageSize, this.page*this.pageSize);
     },
     commentPages() {
       if (this.comments) {
@@ -121,6 +218,35 @@ export default {
         hour: 'numeric',
         minute: 'numeric',
       });
+    },
+    onResolveClicked(commentID) {
+      this.resolveItem = commentID;
+      this.submitted = false;
+      this.resolveDialog = true;
+    },
+    onResolveConfirmed() {
+      console.log('will resolve ' + this.resolveItem);
+      this.resolveDialog = false;
+    },
+    onDeleteClicked(commentID) {
+      this.deleteItem = commentID;
+      this.submitted = false;
+      this.deleteDialog = true;
+    },
+    onDeleteConfirmed() {
+      console.log('will delete ' + this.deleteItem);
+      this.deleteDialog = false;
+    },
+    truncate(text, commentID) {
+      if (text.length > this.maxLength && !this.showFull[commentID]) {
+        return text.substring(0, this.maxLength) + '...';
+      }
+
+      return text;
+    },
+    setFull(commentID) {
+      this.showFull = {};
+      this.showFull[commentID] = true;
     }
   },
   mounted() {
